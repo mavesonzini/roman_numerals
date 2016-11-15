182c56f7da6260e9f2910a3bfbad928e86babd16
Veronica Sonzini <mavesonzini@gmail.com>
2016-11-15 09:36:25 +0000

    First test passing

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,6 @@
+
+  def to_roman(number)
+    if number == 1
+      'I'
+    end
+  end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,9 @@
+require 'roman_numerals.rb'
+
+describe 'RomanNumerals' do
+
+  it "1 to be roman number I" do
+    expect(to_roman(1)).to eq 'I'
+  end
+
+end
